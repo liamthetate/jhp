8865bdd3d497ba21dd4598ffc0a0dfaf73478fa4
Liam Tate <mrtea@protonmail.com>
2022-04-09 10:37:25 +0000

    tried and failed to switch over to postgres, but this version with sqllite works, did a datadump json successfully, now going to try heroku and postgres again. the main thing is this version is solid

--- shop/models.py
+++ shop/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     type = models.CharField(max_length=254)
     dimensions = models.CharField(max_length=254)
-    #image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     #image_url = models.ImageField(null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
