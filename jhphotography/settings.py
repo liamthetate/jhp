847cc51b7cd95a7ceb6841543bf390d34204601a
Liam Tate <mrtea@protonmail.com>
2022-04-11 15:37:40 +0000

    2nd attempt to get heroku to see db changes

--- jhphotography/settings.py
+++ jhphotography/settings.py
@@ -138,6 +138,7 @@ else:
     }
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -207,6 +208,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
