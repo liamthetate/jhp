1201ee3d375b4d1660d9824a55d36e79cee06a3a
Liam Tate <mrtea@protonmail.com>
2022-04-11 16:47:03 +0000

    settings.py removed some commented out code

--- jhphotography/settings.py
+++ jhphotography/settings.py
@@ -138,19 +138,6 @@ else:
     }
 
 
-
-# DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-
-# DATABASES = {
-#     'default': dj_database_url.parse('postgres://xwqbzcwlxdqase:c55c7a4149b321ad2d799cf15f4962e5a1bc014928dd5be0f9af0532a7693a62@ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/dajep1qeqnjlid')
-# } 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
